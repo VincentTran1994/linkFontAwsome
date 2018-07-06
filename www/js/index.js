044eaa61622dee7a179abb3ad6918d4dc8b4705b
Vu Tran <trancongvuit@gmail.com>
2018-07-06 00:17:15 -0400

    working on jquery

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,18 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady(){
-  console.log('devices is ready ...')
+  // reducing texts in cards
+  var fullText=[];
+  var temp = [];
+  for(var  i = 0;i < $("p.card-text").length; i++){
+    fullText[i] = $("p.card-text")[i].innerHTML;
+    temp[i] = $("p.card-text")[i];
+    temp[i].innerHTML = fullText[i].substring(0, 200) + " <span><br>...</span>";
+
+    $("p.card-text")[i].addEventListener("click", function(){
+      $(this)["0"].innerHTML = fullText[temp.indexOf(this)];
+    });
+
+  }
+
 }
